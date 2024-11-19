8dfd00a6320e4cda67b4d31ee5bf61ff8fed5167
JiananAlvin <jiananwang990@gmail.com>
2024-11-19 02:33:43 +0100

    Train successfully

--- job_train_chronacc.sh
+++ job_train_chronacc.sh
@@ -3,12 +3,12 @@
 #SBATCH --ntasks=4               # Adjust tasks (parallel jobs) per node as needed
 #SBATCH --cpus-per-task=4        # CPUs per task, adjust if needed
 #SBATCH --account=vita
-#SBATCH --mem=40G                # Increase memory if needed
-#SBATCH --gres=gpu:3             # Use GPU if supported by the code
+#SBATCH --mem=80G                # Increase memory if needed
+#SBATCH --gres=gpu:4             # Use GPU if supported by the code
 #SBATCH --time=72:00:00          # Set the job to 1 hour max
-#SBATCH --partition=h100         # h100 or 140s
-#SBATCH --output=/home/jiaxu/projects/sp-mdm/job_train_output_chronacc.log
-#SBATCH --error=/home/jiaxu/projects/sp-mdm/job_train_error_chronacc.log
+#SBATCH --partition=l40s         # h100 or l40s
+#SBATCH --output=/home/jiaxu/projects/sp-mdm/job_140s_32bs_output_chronacc.log
+#SBATCH --error=/home/jiaxu/projects/sp-mdm/job_140s_32bs_error_chronacc.log
 #SBATCH --mail-type=END,FAIL,DONE
 #SBATCH --mail-user=jianan.xu@epfl.ch
 
@@ -25,4 +25,4 @@ eval "$(conda shell.bash hook)"
 conda activate ~/venvs/mdm
 
 # Run the Python script
-python -m train.train_mdm --save_dir save/chronacc_my_humanml_trans_enc_512 --dataset humanml --eval_during_training
+python -m train.train_mdm --save_dir save/chronacc_140s_32bs_my_humanml_trans_enc_512 --dataset humanml --eval_during_training
